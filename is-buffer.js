90a262e49f99a8d8fe626d827a1ac2b594372578
hkloudou <hkloudou@gmail.com>
2019-05-01 04:35:41 +0800

    fix

--- is-buffer.js
+++ is-buffer.js
@@ -2,7 +2,7 @@
 module.exports = isBuf;
 
 var withNativeBuffer = typeof Buffer === 'function' && typeof Buffer.isBuffer === 'function';
-var withNativeArrayBuffer = typeof ArrayBuffer === 'function';
+var withNativeArrayBuffer = typeof ArrayBuffer === 'function' || typeof ArrayBuffer === 'object';
 
 var isView = function (obj) {
   return typeof ArrayBuffer.isView === 'function' ? ArrayBuffer.isView(obj) : (obj.buffer instanceof ArrayBuffer);
